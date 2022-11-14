c62eed2b24eae479b2c11959988d5115438edf95
Robert <95116621+beez6239@users.noreply.github.com>
2022-11-14 23:39:43 +0000

    Add files via upload

---
+++ BLL/Variables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Variables
+    {
+        
+        
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+
+    }
+}
